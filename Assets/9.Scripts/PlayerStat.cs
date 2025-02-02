73303893413c3ac5a053d7d5f9cb9dffd2071e92
magotoolivesource <52517277+magotoolivesource@users.noreply.github.com>
2025-02-02 10:58:06 +0900

    hp바 ui 문자와 이미지 변경 처리

--- Assets/9.Scripts/PlayerStat.cs
+++ Assets/9.Scripts/PlayerStat.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStat : MonoBehaviour
 {
 
+    public RectTransform HPImg;
+    public Text HPText;
+    public void UpdateHPBar()
+    {
+
+        float weight = (float)HP / MaxHP;
+        // 스트레츠가 아닐때만 적용 가능
+        //HPImg.anchoredPosition
+        HPImg.sizeDelta = new Vector2(128 * HPImgCount * weight, 128);
+
+        HPText.text = $"{HP}/{MaxHP}";
+    }
+
+
     public int HP;
+    protected int MaxHP;
     public float Def;
+    public int HPImgCount = 5;
 
     public void SetDamage(float p_dmg)
     {
@@ -19,8 +36,9 @@ public class PlayerStat : MonoBehaviour
         }
 
         HP = HP - (int)p_dmg;
+        UpdateHPBar();
 
-        if( HP <= 0)
+        if ( HP <= 0)
         {
             Debug.Log("게임오버");
 
@@ -33,7 +51,8 @@ public class PlayerStat : MonoBehaviour
 
     void Start()
     {
-        
+        MaxHP = HP;
+        UpdateHPBar();
     }
 
 
