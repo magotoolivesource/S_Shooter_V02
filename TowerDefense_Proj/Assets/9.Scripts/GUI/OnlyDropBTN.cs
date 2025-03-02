a16fa50a9b9104d9ef404a261be32de43892ba49
magotoolivesource <52517277+magotoolivesource@users.noreply.github.com>
2025-03-02 17:05:45 +0900

    화면 드래그앤 드랍 처리 부분 작업

--- TowerDefense_Proj/Assets/9.Scripts/GUI/OnlyDropBTN.cs
+++ TowerDefense_Proj/Assets/9.Scripts/GUI/OnlyDropBTN.cs
@@ -1,9 +1,8 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DrngNDropBTN : MonoBehaviour
+public class OnlyDropBTN : MonoBehaviour
     , IBeginDragHandler
     , IEndDragHandler
     , IDragHandler
@@ -16,7 +15,7 @@ public class DrngNDropBTN : MonoBehaviour
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log( $"드래드 시작 : {this.name}");
+        Debug.Log($"드래드 시작 : {this.name}");
         m_InitPos = DragIcon.transform.position;
     }
 
@@ -28,35 +27,35 @@ public class DrngNDropBTN : MonoBehaviour
         DragIcon.transform.position = eventData.position;
     }
 
-    
+
 
     //public Image SendImage = null;
     public void OnDrop(PointerEventData eventData)
     {
+        DrngNDropBTN srcbtn = eventData.selectedObject.GetComponent<DrngNDropBTN>();
         Debug.Log($"드랍처리 : {eventData.selectedObject.name} -> {this.name} ");
 
-        DrngNDropBTN srcbtn = eventData.selectedObject.GetComponent<DrngNDropBTN>();
-        if (srcbtn != null)
+        if(srcbtn != null)
+        {
+            // 복사
+            this.DragIcon.sprite = srcbtn.DragIcon.sprite;
+        }
+
+        // 서로 치환
+        OnlyDropBTN seletedbtn = eventData.selectedObject.GetComponent<OnlyDropBTN>();
+        if (seletedbtn != null)
         {
             // 서로 치환
             Sprite swapsprite = this.DragIcon.sprite;
-            this.DragIcon.sprite = srcbtn.DragIcon.sprite;
-            srcbtn.DragIcon.sprite = swapsprite;
-        }
-
-
-        OnlyDropBTN tempbtn = eventData.selectedObject.GetComponent<OnlyDropBTN>();
-        if (tempbtn != null)
-        {
-            // 지워라
-            tempbtn.DragIcon.sprite = null;
+            this.DragIcon.sprite = seletedbtn.DragIcon.sprite;
+            seletedbtn.DragIcon.sprite = swapsprite;
         }
 
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log( $"드래그 끝 : {this.name}" );
+        Debug.Log($"드래그 끝 : {this.name}");
         DragIcon.transform.position = m_InitPos;//  eventData.pressPosition;
     }
 
@@ -67,6 +66,6 @@ public class DrngNDropBTN : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 }
